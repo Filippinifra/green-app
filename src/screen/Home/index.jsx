ac0bf9e39dc4f83b017452177b4e6cfec15873fb
filippinifra <filippinifra123@gmail.com>
2020-11-14 19:26:57 +0100

    Refactor home

--- src/screen/Home/index.jsx
+++ src/screen/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { ChartLine } from "components/ChartLine";
+import { data } from "./mock";
+import { Badge } from "components/Badge";
+import { Wrapper } from "./styles";
 
-export const Home = () => (
-  <View>
-    <Text>This is the home page</Text>
-  </View>
+export const Home = ({ color }) => (
+  <Wrapper>
+    <Badge color={color} title="Consumi giornalieri nazionali">
+      <ChartLine data={data} height={300} />
+    </Badge>
+  </Wrapper>
 );
---
+++ src/screen/Home/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.ScrollView`
+  padding: 25px 35px;
+`;
