91925c6da3d2be806fc50eb77b34a5c463e26b4a
filippinifra <filippinifra123@gmail.com>
2020-11-15 20:59:12 +0100

    Fix text grammatic

--- src/text/tips.js
+++ src/text/tips.js
@@ -1,8 +1,8 @@
 export const EcoTips = [
   "Oltre il 30% del costo della bolletta dipende dall’utilizzo degli elettrodomestici",
-  "Un elettrodomestico A+++ consuma fino al 60% di energia in meno rispetto alla classe A",
-  "Lavatrice, asciugatrice e lavastoviglie è consigliato farle a pieno carico e basse temperature in fascia serale quando l’energia costa meno",
-  "Le temperature ideale per una casa sono tra 18-20°C durante l’inverno e 20-24°C per l’estate",
-  "Per ogni grado di temperatura sopra 20°C i costi del riscaldamento aumentano del 7-8%",
-  "Un impianto solare termico consente di ridurre i costi di acqua calda e riscaldamento fino al 50%",
+  "Un elettrodomestico A+++ consuma fino al 60% di energia in meno rispetto a un elettrodomestico di classe A",
+  "Lavatrice, asciugatrice e lavastoviglie è consigliato farle a pieno carico e a basse temperature in fascia serale (ovvero quando l’energia costa meno)",
+  "Le temperature ideale per una casa è tra 18-20°C durante l’inverno e tra 20-24°C durante l’estate",
+  "Per ogni grado di temperatura sopra i 20°C i costi di riscaldamento aumentano del 7-8%",
+  "Un impianto solare termico consente di ridurre i costi di acqua calda e del riscaldamento fino al 50%",
 ];
