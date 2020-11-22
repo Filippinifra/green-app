b3568c6f3ba46f6436ea357ff90753ab4c09e4fd
filippinifra <filippinifra123@gmail.com>
2020-11-22 16:44:03 +0100

    Adding refresh layout

--- src/screen/News/index.jsx
+++ src/screen/News/index.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { NewsRow } from "components/NewsRow";
 import { NewsWrapper } from "./styles";
 import { ScrollView } from "react-native-gesture-handler";
 import { mock } from "./mock";
-import { View } from "react-native";
+import { FlatList, RefreshControl, View } from "react-native";
 
-export const News = ({ mainColor, secondColor }) => (
-  <ScrollView>
-    <View style={{ marginBottom: 20, marginTop: 20 }}>
-      {mock.map(({ title, description, url, image }) => (
+export const News = ({ mainColor, secondColor }) => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 3000);
+  };
+
+  return (
+    <FlatList
+      data={mock}
+      renderItem={({ item: { title, description, url, image } }) => (
         <NewsWrapper>
           <NewsRow
             color={secondColor}
@@ -18,7 +28,16 @@ export const News = ({ mainColor, secondColor }) => (
             url={url}
           />
         </NewsWrapper>
-      ))}
-    </View>
-  </ScrollView>
-);
+      )}
+      onEndReached={() => {
+        console.log("end");
+      }}
+      refreshControl={
+        <View style={{ top: 20 }}>
+          <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+        </View>
+      }
+      contentContainerStyle={{ paddingTop: 20, paddingBottom: 20 }}
+    />
+  );
+};
