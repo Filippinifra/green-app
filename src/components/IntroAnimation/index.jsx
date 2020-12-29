f062e7445a49b180bee5a565fb59396f382520f8
filippinifra <filippinifra123@gmail.com>
2020-12-29 01:15:20 +0100

    Replace logo and color background intro to green

--- src/components/IntroAnimation/index.jsx
+++ src/components/IntroAnimation/index.jsx
@@ -1,4 +1,4 @@
-import { COMMON_THIRD_COLOR } from "constants/palette";
+import { THIRD_COLOR } from "constants/palette";
 import React from "react";
 import AnimatedSplash from "react-native-animated-splash-screen";
 
@@ -7,7 +7,7 @@ export const IntroAnimation = ({ children, loaded }) => (
     translucent={true}
     isLoaded={loaded}
     logoImage={require("images/logo.png")}
-    backgroundColor={COMMON_THIRD_COLOR}
+    backgroundColor={THIRD_COLOR}
     logoWidth={80}
     logoHeight={80}
   >
