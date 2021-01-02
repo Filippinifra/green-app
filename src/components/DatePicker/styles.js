7bae0e9ddba3b2c7910f30d158104b23629f542d
filippinifra <filippinifra123@gmail.com>
2021-01-02 23:07:30 +0100

    Remove useless date picker and dependencies

--- src/components/DatePicker/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { COMMON_FIFTH_COLOR } from "constants/palette";
-
-export const PickerWrapper = styled.View`
-  border-bottom-width: 1px;
-  border-color: ${COMMON_FIFTH_COLOR};
-  flex: 1;
-  width: 100%;
-`;
