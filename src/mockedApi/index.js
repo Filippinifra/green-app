7b686876f9f2fc7fe4e2af9d7eb684d77be2a4b4
filippinifra <filippinifra123@gmail.com>
2021-01-02 19:10:07 +0100

    Rework and add new mocker api

--- src/mockedApi/index.js
+++ src/mockedApi/index.js
@@ -1,8 +1,13 @@
-import React from "react";
-
-const { NEWS_ENDPOINT } = require("constants/endpoint");
-const { createServer } = require("miragejs");
-const { newsMock } = require("screen/News/mock");
+import {
+  NEWS_ENDPOINT,
+  CONSUMPTION_TOWNS_ENDPOINT,
+  CONSUMPTION_DATES_ENDPOINT,
+  CONSUMPTION_ENDPOINT,
+} from "constants/endpoint";
+import { createServer } from "miragejs";
+import { newsMock } from "screen/News/mock";
+import { homeMock } from "screen/Home/mock";
+import moment from "moment";
 
 export const runMockServer = () => {
   let server = createServer();
@@ -13,5 +18,27 @@ export const runMockServer = () => {
     if (page === 2) return newsMock.pageTwo;
     if (page === 3) return newsMock.pageThree;
     return [];
+  });
+
+  server.get(CONSUMPTION_TOWNS_ENDPOINT, homeMock.towns);
+
+  server.get(CONSUMPTION_DATES_ENDPOINT, (schema, { queryParams }) => {
+    const { town } = queryParams;
+    if (town === "Milano") return homeMock.datesMilano;
+    if (town === "Roma") return homeMock.datesRoma;
+    if (town === "Napoli") return homeMock.datesNapoli;
+    return [];
+  });
+
+  server.get(CONSUMPTION_ENDPOINT, (schema, { queryParams }) => {
+    const { startDate, endDate } = queryParams;
+
+    const diffDays = moment(endDate).diff(moment(startDate), "days");
+
+    return diffDays === 1
+      ? homeMock.consumptionDay
+      : diffDays > 1 && diffDays < 33
+      ? homeMock.consumptionMonth
+      : homeMock.consumptionYear;
   });
 };
