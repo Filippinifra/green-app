6af8bde1570811e082ca48f0acac72f7050bf835
filippinifra <filippinifra123@gmail.com>
2021-01-02 19:09:14 +0100

    Create DatePicker component

---
+++ src/components/DatePicker/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { TouchElement } from "components/TouchElement";
+import { Text } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import moment from "moment";
+import { PickerWrapper } from "./styles";
+import { useTranslation } from "react-i18next";
+import { COMMON_FIFTH_COLOR } from "constants/palette";
+
+export const DatePicker = ({
+  date,
+  onChange,
+  minimumDate,
+  maximumDate,
+  style,
+  placeholder,
+  disabled,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  const { t } = useTranslation();
+
+  return (
+    <TouchElement
+      onPress={() => {
+        disabled ? null : setVisible(true);
+      }}
+    >
+      <PickerWrapper style={{ ...style }}>
+        {date ? (
+          <Text numberOfLines={1}>{moment(date).format("DD/MM/YYYY")}</Text>
+        ) : (
+          <Text style={{ color: COMMON_FIFTH_COLOR }}>
+            {placeholder || t("general.placeholderPicker")}
+          </Text>
+        )}
+      </PickerWrapper>
+      <DateTimePickerModal
+        isVisible={visible}
+        mode="date"
+        value={new Date(date)}
+        onCancel={() => setVisible(false)}
+        onConfirm={(value) => {
+          onChange(value);
+          setVisible(false);
+        }}
+        minimumDate={minimumDate}
+        maximumDate={maximumDate}
+      />
+    </TouchElement>
+  );
+};
---
+++ src/components/DatePicker/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { COMMON_FIFTH_COLOR } from "constants/palette";
+
+export const PickerWrapper = styled.View`
+  border-bottom-width: 1px;
+  border-color: ${COMMON_FIFTH_COLOR};
+  flex: 1;
+  width: 100%;
+`;
