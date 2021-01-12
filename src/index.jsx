5c6fb267598768aaa65da33c4184d7195f82d6a8
filippinifra <filippinifra123@gmail.com>
2021-01-12 23:31:25 +0100

    Rework index

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { getStorageItem, STORAGE_LANG_KEY } from "utils/storage";
-import i18n from "text/i18n";
 import { IntroAnimation } from "components/IntroAnimation";
 import { Router } from "components/Router";
-import { isMockingServer } from "constants/featureFlag";
-import { runMockServer } from "mockedApi";
-
-if (isMockingServer) {
-  runMockServer();
-}
 
 export const Code = () => {
   const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    getStorageItem(STORAGE_LANG_KEY, (value) => {
-      i18n.changeLanguage(value);
-    });
-  }, []);
 
   useEffect(() => {
     setTimeout(() => setLoaded(true), 2000);
