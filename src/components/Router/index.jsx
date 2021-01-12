80fb12ec189c7f6e5006186f30c14ae1672a06b5
filippinifra <filippinifra123@gmail.com>
2021-01-12 23:30:52 +0100

    Rework Router

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,47 +1,26 @@
-import React, { useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
 import { MainLayout } from "components/MainLayout";
-import { BottomBar } from "components/BottomBar";
-import { PATH_HOME } from "constants/path";
-import { MAP_PATH_TO_CONFIG, DISPOSITON_PATH } from "constants/config";
-import { getStorageItem, STORAGE_LANG_KEY } from "utils/storage";
-import i18n from "text/i18n";
+import { News } from "screen/News";
+import { FIRST_COLOR } from "constants/palette";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { feeds } from "constants/feeds";
 
-const Tab = createBottomTabNavigator();
+import { NavigationContainer } from "@react-navigation/native";
 
-export const Router = () => {
-  useEffect(() => {
-    getStorageItem(STORAGE_LANG_KEY, (value) => {
-      i18n.changeLanguage(value);
-    });
-  }, []);
+const Drawer = createDrawerNavigator();
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={PATH_HOME}
-        tabBar={(props) => <BottomBar {...props} />}
-      >
-        {DISPOSITON_PATH.map((path, index) => {
-          const {
-            Component,
-            tabTitle,
-            mainColor,
-            secondColor,
-          } = MAP_PATH_TO_CONFIG[path];
-
-          return (
-            <Tab.Screen name={path} key={`tab-${index}`}>
-              {() => (
-                <MainLayout nameRoute={tabTitle} colorHeader={mainColor}>
-                  <Component mainColor={mainColor} secondColor={secondColor} />
-                </MainLayout>
-              )}
-            </Tab.Screen>
-          );
-        })}
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
+export const Router = () => (
+  <NavigationContainer>
+    <Drawer.Navigator>
+      {feeds.map(({ name, url }) => (
+        <Drawer.Screen name={name}>
+          {() => (
+            <MainLayout colorHeader={FIRST_COLOR}>
+              <News mainColor={FIRST_COLOR} url={url} />
+            </MainLayout>
+          )}
+        </Drawer.Screen>
+      ))}
+    </Drawer.Navigator>
+  </NavigationContainer>
+);
