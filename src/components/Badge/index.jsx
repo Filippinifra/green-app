4a0d6e9669e23f3b03c41ccd8762cb289abd454c
filippinifra <filippinifra123@gmail.com>
2021-01-12 23:30:17 +0100

    Rework badge

--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 import { ShadowStyle } from "components/Shadow";
-import { BadgeWrapper, HeaderWrapper, Title } from "./styles";
+import { BadgeWrapper, HeaderWrapper, Title, Line } from "./styles";
 
-export const Badge = ({ children, title, color }) => (
+export const Badge = ({ children, title, color, numberOfLines }) => (
   <View style={ShadowStyle.ShadowBox}>
     <BadgeWrapper color={color}>
       <View>
         <HeaderWrapper color={color} s>
-          <Title>{title}</Title>
+          <Title numberOfLines={numberOfLines}>{title}</Title>
         </HeaderWrapper>
+        <Line />
         {children}
       </View>
     </BadgeWrapper>
