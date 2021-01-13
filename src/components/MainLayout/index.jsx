498a87131f2f2619032142baac6e551f3d0c5e3c
filippinifra <filippinifra123@gmail.com>
2021-01-13 20:09:30 +0100

    Revert "Rework MainLayout"
    This reverts commit 384f42c2e692e371338b2ff40710fb1f50ba417a.

--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -1,15 +1,45 @@
 import React from "react";
-import { View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { COMMON_FIRST_COLOR } from "constants/palette";
+import { Dimensions, View } from "react-native";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { COMMON_THIRD_COLOR } from "constants/palette";
+import {
+  HEIGHT_BOTTOM_NAVIGATOR,
+  HEIGHT_TOP_TITLE_ROUTE_NAME,
+} from "constants/config";
+import { FlagChangeBox } from "components/FlagChangeBox";
+import { Title, RightBoxHeader } from "./styles";
 
-export const MainLayout = ({ children, colorHeader }) => {
+export const MainLayout = ({ children, nameRoute, colorHeader }) => {
+  const insets = useSafeAreaInsets();
+  const { height } = Dimensions.get("window");
+  const ViewHeight =
+    height -
+    (HEIGHT_BOTTOM_NAVIGATOR +
+      insets.bottom +
+      insets.top +
+      HEIGHT_TOP_TITLE_ROUTE_NAME);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: colorHeader }}>
       <View
         style={{
-          flex: 1,
-          backgroundColor: COMMON_FIRST_COLOR,
+          height: HEIGHT_TOP_TITLE_ROUTE_NAME,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Title>{nameRoute.toUpperCase()}</Title>
+        <RightBoxHeader>
+          <FlagChangeBox />
+        </RightBoxHeader>
+      </View>
+      <View
+        style={{
+          height: ViewHeight,
+          backgroundColor: COMMON_THIRD_COLOR,
         }}
       >
         {children}
