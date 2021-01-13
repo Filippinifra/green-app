725b5df9273475143bed60f2f5aa65b72e79ede3
filippinifra <filippinifra123@gmail.com>
2021-01-13 20:09:24 +0100

    Revert "Rework Router"
    This reverts commit 80fb12ec189c7f6e5006186f30c14ae1672a06b5.

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,26 +1,47 @@
-import React from "react";
-import { MainLayout } from "components/MainLayout";
-import { News } from "screen/News";
-import { FIRST_COLOR } from "constants/palette";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { feeds } from "constants/feeds";
-
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MainLayout } from "components/MainLayout";
+import { BottomBar } from "components/BottomBar";
+import { PATH_HOME } from "constants/path";
+import { MAP_PATH_TO_CONFIG, DISPOSITON_PATH } from "constants/config";
+import { getStorageItem, STORAGE_LANG_KEY } from "utils/storage";
+import i18n from "text/i18n";
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
-export const Router = () => (
-  <NavigationContainer>
-    <Drawer.Navigator>
-      {feeds.map(({ name, url }) => (
-        <Drawer.Screen name={name}>
-          {() => (
-            <MainLayout colorHeader={FIRST_COLOR}>
-              <News mainColor={FIRST_COLOR} url={url} />
-            </MainLayout>
-          )}
-        </Drawer.Screen>
-      ))}
-    </Drawer.Navigator>
-  </NavigationContainer>
-);
+export const Router = () => {
+  useEffect(() => {
+    getStorageItem(STORAGE_LANG_KEY, (value) => {
+      i18n.changeLanguage(value);
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={PATH_HOME}
+        tabBar={(props) => <BottomBar {...props} />}
+      >
+        {DISPOSITON_PATH.map((path, index) => {
+          const {
+            Component,
+            tabTitle,
+            mainColor,
+            secondColor,
+          } = MAP_PATH_TO_CONFIG[path];
+
+          return (
+            <Tab.Screen name={path} key={`tab-${index}`}>
+              {() => (
+                <MainLayout nameRoute={tabTitle} colorHeader={mainColor}>
+                  <Component mainColor={mainColor} secondColor={secondColor} />
+                </MainLayout>
+              )}
+            </Tab.Screen>
+          );
+        })}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
