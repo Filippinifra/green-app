8b22acfb51eec1a74fdb238e373b0656f340734a
filippinifra <filippinifra123@gmail.com>
2021-01-13 20:09:17 +0100

    Revert "Add feed and palette const file"
    This reverts commit 1da21f58ce981a11a144ea39e6ac0b99ed03a6d3.

--- src/constants/palette.js
+++ src/constants/palette.js
@@ -1,15 +1,22 @@
-export const COMMON_FIRST_COLOR = "#ffffff";
-export const COMMON_SECOND_COLOR = "lightgray";
+export const COMMON_FIRST_COLOR = "#000000";
+export const COMMON_SECOND_COLOR = "#3D3D3D";
+export const COMMON_THIRD_COLOR = "#ffffff";
+export const COMMON_FOURTH_COLOR = "#777777";
+export const COMMON_FIFTH_COLOR = "lightgray";
 export const COMMON_ERROR_COLOR = "red";
+export const COMMON_WARNING_COLOR = "yellow";
 
-export const FIRST_COLOR = "#007DF7"; //BLUE
+export const FIRST_COLOR = "#1E8BD9"; //BLUE
+export const SECOND_COLOR = "#90BEDE";
 
-export const RAINBOW_COLORS = [
-  "red",
-  "orange",
-  "#FFC300",
-  "limegreen",
-  "green",
-  "blue",
-  "purple",
-];
+export const THIRD_COLOR = "#628F00"; //GREEN
+export const FOURTH_COLOR = "#A5CB52";
+
+export const FIFTH_COLOR = "#673187"; //PURPLE
+export const SIXTH_COLOR = "#C68DE2";
+
+export const SEVENTH_COLOR = "#946C38"; //BROWN
+export const EIGHTH_COLOR = "#B9ABA2";
+
+export const NINETH_COLOR = "#D65108"; //ORANGE
+export const TENTH_COLOR = "#EFA00B";
